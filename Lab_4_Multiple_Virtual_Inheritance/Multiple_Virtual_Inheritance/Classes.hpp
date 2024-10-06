53583aac09d51bc9f41b452454830e134ba90fec
shoodymon <shoody2016@yandex.ru>
2024-10-06 21:56:32 +0300

    ЛР4
    Наследование. Множественное наследование, виртуальное наследование

---
+++ Lab_4_Multiple_Virtual_Inheritance/Multiple_Virtual_Inheritance/Classes.hpp
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+class B {
+protected:
+	std::string m_name;
+	int m_age;
+
+public:
+	B(const std::string& name, int age) : m_name(name), m_age(age) {}
+	virtual ~B() = default; // для корректного удаления объектов через указатели на базовый класс
+
+	virtual void show() const {
+		std::cout << "\n\t\tB: " << "\n\t---------------" << "\n\tИмя: " << m_name << "\n\tВозраст: " << m_age << "\n\t---------------" << std::endl;
+	}
+};
+
+class D1 : public B {
+public:
+	D1(const std::string& name, int age) : B(name, age) {}
+
+	virtual void show() const override {
+		std::cout << "\n\t\tD1: " << "\n\t---------------" << "\n\tИмя: " << m_name << "\n\tВозраст: " << m_age << "\n\t---------------" << std::endl;
+	}
+};
+
+class D2 : private B {
+public:
+	D2(const std::string& name, int age) : B(name, age) {}
+
+	virtual void show() const override {
+		std::cout << "\n\t\tD2: " << "\n\t---------------" << "\n\tИмя: " << m_name << "\n\tВозраст: " << m_age << "\n\t---------------" << std::endl;
+	}
+};
+
+class D3 : private D1, public D2 {
+public:
+	D3(const std::string& name1, int age1, const std::string& name2, int age2) : D1(name1, age1), D2(name2, age2) {}
+
+	virtual void show() const override {
+		D1::show();
+		D2::show();
+	}
+};
---
+++ Lab_4_Multiple_Virtual_Inheritance/Multiple_Virtual_Inheritance/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Classes.hpp"
+
+int main() {
+	setlocale(0, "");
+	system("chcp1251");
+	system("cls");
+
+	B obj1("Mark", 12);
+	D1 obj2("Steven", 15);
+	D2 obj3("Travis", 64);
+	D3 obj4("Hooch", 43, "George", 37);
+
+	std::cout << "\n\tДемонстрация работы: " << std::endl;
+
+	obj1.show();
+	obj2.show();
+	obj3.show();
+	obj4.show();
+}
